871d9f2c57112f02d832e239765d5bc056ff7737
Ben Orchard <thefirstmuffinman@gmail.com>
2014-11-14 12:05:18 +0000

    initial commit
    sort works, 2 input systems

---
+++ Ballot.java
@@ -0,0 +1,240 @@
+package novemberAssessment;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ballot {
+	
+	public Candidate[] candidates;
+	
+	// tracks year group of current election
+	private int yearGroup;
+
+	private Scanner scanner;
+	
+	public Ballot() {
+		scanner = new Scanner(System.in);
+	}
+	
+	private String getLine(String prompt) {
+		boolean badInput = false;
+		String line;
+		
+		do {
+			badInput = false;
+			
+			System.out.print(prompt);
+			line = scanner.nextLine();
+			
+			if (line.isEmpty()) {
+				System.out.println("ERROR: nothing entered");
+				badInput = true;
+			}
+		} while (badInput);
+		
+		return line;
+	}
+	
+	/**
+	 * Try to get a positive integer from stdin.
+	 * 
+	 * If an integer is not entered,
+	 * @return	the number entered. If an integer is not entered,
+	 * 			return -2. If it is not positive, return -1.
+	 */
+	private int getPositiveInt() {		
+		int num;
+		
+		try {
+			num = new Scanner(System.in).nextInt();
+		} catch (InputMismatchException e) {
+			return -2;
+		}
+		
+		if (num < 0) { return -1; } else { return num; }
+	}
+	
+	/**
+	 * Force a positive integer to be entered and return it.
+	 * 
+	 * @param prompt	The string to display before asking for input.
+	 * 					No newlines nor spaces are appended.
+	 * @return			The (valid) integer entered.
+	 */
+	private int promptPositiveInt(String prompt) {		
+		boolean badInput;
+		int num;
+		
+		do {
+			badInput = false;
+			
+			System.out.print(prompt);
+			num = this.getPositiveInt();
+			
+			if (num == -2) {
+				System.out.println("ERROR: not an integer");
+				badInput = true;
+			} else if (num == -1) {
+				System.out.println("ERROR: not a positive integer");
+				badInput = true;
+			}
+		} while (badInput);
+		
+		return num;
+	}
+	
+	/**
+	 * Set the candidates for the next election.
+	 * 
+	 * Note: this does not *add* candidates to the current list.
+	 * The list of candidates is rewritten (as this would
+	 * be used after every election to do another one).
+	 */
+	public void setCandidates(int numOfCandidates) {
+		this.candidates = new Candidate[numOfCandidates];
+		
+		this.yearGroup = this.promptPositiveInt("Candidate year group: ");
+		
+		for (int i = 0; i < numOfCandidates; i++) {
+			// prompt for name
+			String name = this.getLine("Candidate " + (i + 1) + "/" + numOfCandidates + " name: ");
+			
+			this.candidates[i] = new Candidate(name, yearGroup);
+			this.candidates[i].prefs = new int[numOfCandidates];
+		}
+	}
+	
+	private void printOnPaper(String string) {
+		System.out.println("| " + string);
+	}
+	
+	public void makeVote() {
+		/** Do a ballot paper. */
+		
+		// some fun ascii art
+		String ballotPaperArt = ""
+				+ "\n------------------------------------"
+				+ "\n|           BALLOT PAPER"
+				+ "\n|             YEAR " + this.yearGroup
+				+ "\n|";
+		System.out.println(ballotPaperArt);
+		this.voteSystem2();
+	}
+	
+	/**
+	 * Vote using input type 1.
+	 * 
+	 * I guess this method isn't entirely clear, but I prefer it.
+	 * This input method asks for your preferences in order and takes
+	 * input in the form of a number which corresponds to a candidate
+	 * on the ballot paper. It's a bit backwards and requires some
+	 * slightly different thinking in the programming, but isn't that
+	 * what you'd do IRL: mark your first preference first, followed by
+	 * your second and third... ? ¯\_(ツ)_/¯
+	 */
+	public void voteSystem1() {
+		// print candidate names & associated number
+		for (int i = 0; i < this.candidates.length; i++) {
+			this.printOnPaper((i + 1) + ". [ ] " + this.candidates[i].name);
+		}
+		this.printOnPaper("");
+		
+		// instructions because not the clearest input system immediately
+		this.printOnPaper("Please enter the number of the associated candidate for each of your preferences.");
+		this.printOnPaper("");
+		
+		// Maps choices to candidates based on the order of this.candidates.
+		// Position is points, value is candidate.
+		// i.e. given {4, 2, 1, 3, 5}, the 4th candidate on the paper would be the favourite.
+		// we load them into an array to give them a chance to change their mind later
+		int[] choices = new int[this.candidates.length];
+		
+		for (int i = 0; i < this.candidates.length; i++) {
+			choices[i] = this.promptPositiveInt("| Preference " + (i + 1) + ": ") - 1;
+		}
+		
+		// TODO: show the current state
+		// TODO: later: could be all ascii art-ified :D
+		
+		// TODO: check whether they're okay with
+		
+		for (int i = 0; i < this.candidates.length; i++) {
+			this.candidates[choices[i]].prefs[i] += 1;
+			//System.out.println(this.candidates[choices[i]].name + " ranked as " + (i + 1));
+		}
+	}
+	
+	/**
+	 * Vote using input type 2.
+	 * 
+	 * Input type 1 feels easier to do IMHO, but I'm a programmer, so
+	 * I shouldn't assume to know anything about my clients ;)
+	 * This input method prints each candidate and asks for your
+	 * preference.
+	 */
+	public void voteSystem2() {
+		// print candidate names
+		for (int i = 0; i < this.candidates.length; i++) {
+			this.printOnPaper("[ ] " + this.candidates[i].name);
+		}
+		this.printOnPaper("");
+		
+		this.printOnPaper("Please enter your preference for each of the candidates.");
+		this.printOnPaper("");
+		
+		// Position is candidate, value is their rank.
+		// i.e. given {4, 2, 1, 3, 5}, the 4th candidate on the paper
+		// would be the 3rd ranked.
+		// we load them into an array to give them a chance to change
+		// their mind later
+		int[] choices = new int[this.candidates.length];
+		
+		for (int i = 0; i < this.candidates.length; i++) {
+			choices[i] = this.promptPositiveInt("| Cand. " + (i + 1) + ", " + this.candidates[i].name + ": ");
+		}
+		
+		// TODO: show the current state
+		// TODO: later: could be all ascii art-ified :D
+		
+		// TODO: check whether they're okay with
+		
+		for (int i = 0; i < this.candidates.length; i++) {
+			this.candidates[i].prefs[choices[i]] += 1;
+			//System.out.println(this.candidates[i].name + " ranked as " + (choices[i] + 1));
+		}
+	}
+	
+	public void printResults() {
+		for (int i = 0; i < this.candidates.length; i++) {
+			System.out.println(this.candidates[i].name + " points: " + this.candidates[i].totalPoints());
+		}
+	}
+	
+	public void endElection() {
+		// TODO: functionise??
+		// sort by point score, low -> high
+		Arrays.sort(this.candidates);
+		// because we're only finding the winner, only check the first
+		// 2 for being identical
+		if (this.candidates[0].compareTo(this.candidates[1]) == 0) {
+			System.out.println("Draw! Re-election time!");
+			return;
+		}
+		this.printResults();
+		System.out.println("The winner for Year " + this.yearGroup + " is... " + this.candidates[0].name + ", with " + this.candidates[0].totalPoints() + " points!");
+	}
+	
+	private void runTests() {
+		this.setCandidates(5);
+		this.makeVote();
+		this.makeVote();
+		this.endElection();
+	}
+	
+	public static void main(String[] args) {
+		Ballot test = new Ballot();
+		test.runTests();
+	}
+
+}
---
+++ Candidate.java
@@ -0,0 +1,76 @@
+package novemberAssessment;
+
+public class Candidate implements Comparable<Candidate> {
+
+	public String name;
+	public int yearGroup;
+	
+	// we have to store each preference in a separate variable to
+	// compare in case of a tie
+	// pref 1 (most favourite) = prefs[0] etc.
+	// (array is programatically way easier to use than separate vars, see Ballot.setVotes())
+	public int[] prefs;
+	
+	public Candidate(String name, int yearGroup) {
+		this.name = name;
+		this.yearGroup = yearGroup;
+	}
+	
+	/**
+	 * Return the candidate's total points.
+	 * 
+	 * Total number of points is equal to the sum of the value of each preference:
+	 *     preference 1 (most favourite) = 1 point
+	 *     preference 5 (least favourite) = 5 points
+	 *     so e.g. {2, 4, 0} = 1(2) + 2(4) + 3(0) = 2 + 8 = 10
+	 * A lower point total point value is better.
+	 * 
+	 * @return The candidate's total points.
+	 */
+	public int totalPoints() {
+		// Return the total points of the candidate.
+		
+
+		
+		int total = 0;
+		
+		for (int i = 0; i < prefs.length; i++) {
+			total += prefs[i] * (i + 1);
+		}
+		
+		return total;
+	}
+
+	@Override
+	/**
+	 * Helper function used by Arrays.sort() to order Candidates.
+	 * 
+	 * The specification says that a candidate beats another if they
+	 * have a lower point score. If they are equal, the candidate with
+	 * the most 1st preference votes wins. If *they* are equal, it's
+	 * down to the 2nd preferences votes, and so on.
+	 * 
+	 * We return 0 *if and only if* the candidates' scores are truly
+	 * identical (i.e. exact same votes).
+	 */
+	public int compareTo(Candidate otherCandidate) {
+		if (this.totalPoints() > otherCandidate.totalPoints()) { return 1; }
+		if (this.totalPoints() < otherCandidate.totalPoints()) { return -1; }
+		// point score is equal -- begin checking separate preference votes
+		else {
+			for (int i = 0; i < this.prefs.length; i++) {
+				if (this.prefs[i] > otherCandidate.prefs[i]) { return 1; }
+				if (this.prefs[i] > otherCandidate.prefs[i]) { return -1; }
+				// if this level of preference votes was equal,
+				// continue to next level
+				// TODO: recursion instead? but we need to check total
+				//       points first, so the recursion would be in a
+				//       helper function
+				else continue;
+			}
+			// if we got to the end of the preference rankings, the
+			// candidates are exactly the same
+			return 0;
+		}
+	}
+}
